aaa4598771231c5e252cf9c1194bf194befbaca2
Jonathan Alvarez G <j@jonalvarezz.com>
2018-11-27 22:58:06 -0500

    Add Button comp

---
+++ src/components/Button.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import Context from '../theme'
+import styled, { css } from 'styled-components'
+
+const Button = styled.button`
+  background: transparent;
+  text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
+  line-height: 1.499;
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  padding: 0 15px;
+  font-size: 14px;
+  border-radius: 4px;
+  height: 32px;
+
+  ${props =>
+    props.danger
+      ? css`
+          border-color: ${props.theme.red};
+          color: ${props.theme.red};
+
+          &:hover {
+            background-color: ${props.theme.red};
+            color: white;
+          }
+        `
+      : css`
+          border-color: ${props.theme.darkGray};
+          color: ${props.theme.darkGray};
+
+          &:hover {
+            background-color: ${props.theme.darkGray};
+            color: white;
+          }
+        `}
+`
+
+const StyledButton = props => {
+  const theme = useContext(Context)
+  return <Button theme={theme} type="button" {...props} />
+}
+
+export default StyledButton
